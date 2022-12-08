c3799bc0f64fc2d73b7ffa7cb35a780d2917e3ed
Daniil Borovov <borovovdaniil@gmail.com>
2022-12-09 00:28:12 +0600

    Open/close popup. Load values to form and save to markup

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
+// Секция выбора нужных элементов на странице
+const popup = document.querySelector('.popup');
+const editFormSaveSaveBtn = popup.querySelector('.edit-form__btn-save');
+const popupCloseBtn = popup.querySelector('.popup__close');
+const editProfileBtn = document.querySelector('.profile__edit-button');
+const elements = document.querySelector('.elements');
+
+// Функции
+
 // Функция переключения видимости попапа
 function togglePopup() {
-  let popup = document.querySelector('.popup');
   popup.classList.toggle('popup_opened');
 }
 
+// Функция подгрузки текстового значения originClass в поле формы с name = destName
+function loadDataToField(originClass, destName) {
+  document.querySelector(`input[name="${destName}"]`).value = document.querySelector(`.${originClass}`).textContent;
+}
+
+// Функция вставки поля originName в элемент с классом destClass
+function loadDataToPage(originName, destClass) {
+  document.querySelector(`.${destClass}`).textContent = document.querySelector(`input[name="${originName}"]`).value;
+}
+
+// Привязка функций к кнопкам
+
 // Привязка функции togglePopup к кнопкам на верстке
-let popupCloseBtn = document.querySelector('.popup__close');
-let editProfileBtn = document.querySelector('.profile__edit-button');
 popupCloseBtn.addEventListener('click', togglePopup);
 editProfileBtn.addEventListener('click', togglePopup);
 
+// Привязка функции loadDataToField к кнопке редактирования профиля
+editProfileBtn.addEventListener('click', () => {
+  loadDataToField('profile__name', 'name');
+  loadDataToField('profile__job', 'job');
+});
+
+// Привязка функции к кнопке сохранить
+editFormSaveSaveBtn.addEventListener('click', () => {
+  loadDataToPage('name', 'profile__name');
+  loadDataToPage('job', 'profile__job');
+});
+
 // Функция переключения лайков
-let elements = document.querySelector('.elements'); // выбираем блок elements целиком
-elements.addEventListener('click', ({target}) => {
+elements.addEventListener('click', ({ target }) => {
   if (target.classList.contains('element__like')) { // если кликаем на элемент like то
     target.classList.toggle('element__like_active');// ставим убираем модификатор активности
   }
-}); 
+});
+
