afc0928b4a79ad5b1bee538b7d481ca07c7a6924
Daniil Borovov <borovovdaniil@gmail.com>
2022-12-14 20:15:35 +0600

    Fix review 1 bugs

--- js/index.js
+++ js/index.js
@@ -1,44 +1,28 @@
 // Секция выбора нужных элементов на странице
+// Константы и переменные
 const popup = document.querySelector('.popup');
-const editFormSaveSaveBtn = popup.querySelector('.edit-form__btn-save');
 const popupCloseBtn = popup.querySelector('.popup__close');
 const editProfileBtn = document.querySelector('.profile__edit-button');
 const elements = document.querySelector('.elements');
+const formElement = document.querySelector('.edit-form');
+let nameInput = formElement.querySelector(`input[name='name']`);
+let jobInput = formElement.querySelector(`input[name='job']`);
+const profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileJob = profile.querySelector('.profile__job');
 
 // Функции
+
+// Функция подгрузки значений Name и Job из верстки в поля формы
+function fillFormFields() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
 
 // Функция переключения видимости попапа
 function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
-
-// Функция подгрузки текстового значения originClass в поле формы с name = destName
-function loadDataToField(originClass, destName) {
-  document.querySelector(`input[name="${destName}"]`).value = document.querySelector(`.${originClass}`).textContent;
-}
-
-// Функция вставки поля originName в элемент с классом destClass
-function loadDataToPage(originName, destClass) {
-  document.querySelector(`.${destClass}`).textContent = document.querySelector(`input[name="${originName}"]`).value;
-}
-
-// Привязка функций к кнопкам
-
-// Привязка функции togglePopup к кнопкам на верстке
-popupCloseBtn.addEventListener('click', togglePopup);
-editProfileBtn.addEventListener('click', togglePopup);
-
-// Привязка функции loadDataToField к кнопке редактирования профиля
-editProfileBtn.addEventListener('click', () => {
-  loadDataToField('profile__name', 'name');
-  loadDataToField('profile__job', 'job');
-});
-
-// Привязка функции к кнопке сохранить
-editFormSaveSaveBtn.addEventListener('click', () => {
-  loadDataToPage('name', 'profile__name');
-  loadDataToPage('job', 'profile__job');
-});
 
 // Функция переключения лайков
 elements.addEventListener('click', ({ target }) => {
@@ -47,3 +31,46 @@ elements.addEventListener('click', ({ target }) => {
   }
 });
 
+// Функция обработчика отправки формы
+function handleFormSubmit (evt) {
+  // Отменить стандартное поведение
+  evt.preventDefault(); 
+  // Заменить данные в верстке
+  profileName.textContent = nameInput.value;
+  profileJob.textContent =  jobInput.value;
+  togglePopup();
+}
+
+// // Функция подгрузки текстового значения originClass в поле формы с name = destName
+// function loadDataToField(originClass, destName) {
+//   document.querySelector(`input[name="${destName}"]`).value = document.querySelector(`.${originClass}`).textContent;
+// }
+
+// // Функция вставки поля originName в элемент с классом destClass
+// function loadDataToPage(originName, destClass) {
+//   document.querySelector(`.${destClass}`).textContent = document.querySelector(`input[name="${originName}"]`).value;
+// }
+
+// Привязка функций к кнопкам
+
+// Привязка функции togglePopup к кнопкам на верстке
+popupCloseBtn.addEventListener('click', togglePopup);
+editProfileBtn.addEventListener('click', togglePopup);
+
+// Прикрепляем обработчик к форме при отправке
+formElement.addEventListener('submit', handleFormSubmit);
+
+// // Привязка функции loadDataToField к кнопке редактирования профиля
+// editProfileBtn.addEventListener('click', () => {
+//   loadDataToField('profile__name', 'name');
+//   loadDataToField('profile__job', 'job');
+// });
+
+
+
+
+
+
+
+
+fillFormFields();
