e42edaa0ce3c56c924578bf33b1d0c71b32806f4
Daniil Borovov <superskillbro@gmail.com>
2022-12-23 20:12:33 +0600

    Render cards from array. Remove cards from markup

--- js/index.js
+++ js/index.js
@@ -9,8 +9,45 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.edit-form');
+const cardTemplate = document.querySelector('#card').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 // Функции
+
+// Функция создание карточек внутри elements из переданного массива
+function renderCardsFromArray(arr) {
+  arr.forEach(element => {
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__image').src = element.link;
+    cardElement.querySelector('.element__title').textContent = element.name;
+    elements.append(cardElement);
+  });
+}
 
 // Функция подгрузки значений Name и Job из верстки в поля формы
 function fillFormFields() {
@@ -34,6 +71,9 @@ function handleFormSubmit (evt) {
 }
 
 // Привязка функций к кнопкам
+
+// Создание карточек из массива
+renderCardsFromArray(initialCards);
 
 // Привязка функции togglePopup к кнопкам на верстке
 popupCloseBtn.addEventListener('click', togglePopup);
